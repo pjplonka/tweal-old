db68755e8ed3fd4d24deb2dc8be8387d6fc454c5
Paweł Płonka <pjplonka@gmail.com>
2021-05-30 13:11:26 +0200

    Split article form to two columns, add placeholder to redactor and fix value for category id select element.

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -1,21 +1,28 @@
-@include('common.input-text', ['id' => 'title', 'label' => 'Article title', 'value' => old('title') ?: $article->title])
+<div class="row">
 
-@if (isset($edit))
-    @include('common.input-text', ['id' => 'slug', 'label' => 'Article slug', 'value' => old('slug') ?: $article->slug])
-@endif
+    <div class="col-lg-9">
+        @include('common.input-text', ['id' => 'title', 'label' => 'Article title', 'value' => old('title') ?: $article->title])
 
-@include('common.select', [
-    'id' => 'category_id',
-    'label' => 'Category',
-    'value' => old('company_id') ?: $article->category_id,
-    'default' => 'Select category',
-    'hint' => 'Category name.',
-    'options' => $categories->map(function($category) {
-        return [
-            'value' => $category->id,
-             'label' => $category->name,
-        ];
-    })
-])
+        @if (isset($edit))
+            @include('common.input-text', ['id' => 'slug', 'label' => 'Article slug', 'value' => old('slug') ?: $article->slug])
+        @endif
 
-@include('common.redactor', ['id' => 'content', 'value' => old('content') ?: $article->content])
+        @include('common.redactor', ['id' => 'content', 'value' => old('content') ?: $article->content, 'placeholder' => 'Write your article content here...'])
+    </div>
+
+    <div class="col-lg-3">
+        @include('common.select', [
+            'id' => 'category_id',
+            'label' => 'Category',
+            'value' => old('category_id') ?: $article->category_id,
+            'default' => 'Select category',
+            'hint' => 'Category name.',
+            'options' => $categories->map(function($category) {
+                return [
+                    'value' => $category->id,
+                     'label' => $category->name,
+                ];
+            })
+        ])
+    </div>
+</div>
